952d3f29c33d7d48ad35c4eae7217811ab8cfa40
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-09 20:06:51 +0530

    Adding UC3

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -11,14 +11,16 @@ namespace BinarySearchTree
         public T nodeData { get; set; }
         public MyBinarySearchTree<T> leftTree { get; set; }
         public MyBinarySearchTree<T> rightTree { get; set; }
+
+        int leftCount = 0, rightCount = 0;
+        public bool result = false;
+
         public MyBinarySearchTree(T nodeData)
         {
             this.nodeData = nodeData;
             this.leftTree = null;
             this.rightTree = null;
         }
-        int leftCount = 0, rightCount = 0;
-
         public void Insert(T data)
         {
             T currentNodeValue = nodeData;
@@ -66,6 +68,32 @@ namespace BinarySearchTree
         public void GetSIze()
         {
             Console.WriteLine("\nSize of Binary Tree is: " + (this.leftCount + this.rightCount + 1));
+        }
+
+        public bool IfExists(T element, MyBinarySearchTree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.nodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST: " + node.nodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in BST", node.nodeData);
+            }
+            if (element.CompareTo(node.nodeData) < 0)
+            {
+                IfExists(element, node.leftTree);
+            }
+            if (element.CompareTo(node.nodeData) > 0)
+            {
+                IfExists(element, node.rightTree);
+            }
+            return result;
         }
     }
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -13,23 +13,47 @@ namespace BinarySearchTree
             Console.WriteLine("Welcome to Binary Search Tree");
 
             MyBinarySearchTree<int> binaryTree = new MyBinarySearchTree<int>(56);
-            binaryTree.Insert(30);
-            binaryTree.Insert(70);
-            binaryTree.Insert(22);
-            binaryTree.Insert(40);
-            binaryTree.Insert(60);
-            binaryTree.Insert(95);
-            binaryTree.Insert(11);
-            binaryTree.Insert(65);
-            binaryTree.Insert(3);
-            binaryTree.Insert(16);
-            binaryTree.Insert(6);
-            binaryTree.Insert(67);
-
-
-            binaryTree.Display();
-            binaryTree.GetSIze();
-            Console.ReadKey();
+            while (true)
+            {
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("\n1. Create Binary Search Tree \n2. Display Binary Search Tree elements \n3. Check Size of BST \n4. Search a value in BST \n5. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        binaryTree.Insert(30);
+                        binaryTree.Insert(70);
+                        binaryTree.Insert(22);
+                        binaryTree.Insert(40);
+                        binaryTree.Insert(60);
+                        binaryTree.Insert(95);
+                        binaryTree.Insert(11);
+                        binaryTree.Insert(65);
+                        binaryTree.Insert(3);
+                        binaryTree.Insert(16);
+                        binaryTree.Insert(63);
+                        binaryTree.Insert(67);
+                        break;
+                    case 2:
+                        binaryTree.Display();
+                        break;
+                    case 3:
+                        binaryTree.GetSIze();
+                        break;
+                    case 4:
+                        int search = 67;
+                        bool result = binaryTree.IfExists(search, binaryTree);
+                        if (result == true)
+                            Console.WriteLine("{0} is present in the Binary Search Tree", search);
+                        else
+                            Console.WriteLine("{0} is not present in the Binary Search Tree", search);
+                        break;
+                    default:
+                        Console.WriteLine("Please select the correct option");
+                        break;
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
