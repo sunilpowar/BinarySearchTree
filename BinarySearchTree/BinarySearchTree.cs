311a15dd82f56ad1a988b628a23fc3812584d121
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-09 19:59:39 +0530

    Adding UC1

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class MyBinarySearchTree<T> where T : IComparable<T>
+    {
+        public T nodeData { get; set; }
+        public MyBinarySearchTree<T> leftTree { get; set; }
+        public MyBinarySearchTree<T> rightTree { get; set; }
+        public MyBinarySearchTree(T nodeData)
+        {
+            this.nodeData = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+
+        public void Insert(T data)
+        {
+            T currentNodeValue = nodeData;
+            if ((data.CompareTo(currentNodeValue)) < 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new MyBinarySearchTree<T>(data);
+                }
+                else
+                    this.leftTree.Insert(data);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new MyBinarySearchTree<T>(data);
+                }
+                else
+                    this.rightTree.Insert(data);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                leftCount++;
+                this.leftTree.Display();
+            }
+            Console.Write(this.nodeData.ToString() + " ");
+            if (this.rightTree != null)
+            {
+                rightCount++;
+                this.rightTree.Display();
+            }
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -12,6 +12,11 @@ namespace BinarySearchTree
         {
             Console.WriteLine("Welcome to Binary Search Tree");
 
+            MyBinarySearchTree<int> binaryTree = new MyBinarySearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+
             Console.ReadKey();
         }
     }
