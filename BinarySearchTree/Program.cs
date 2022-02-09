e5e2d432ba5f527860fb7b15aa6a3cf73872f3f7
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-09 13:27:47 +0530

    Adding Welcome message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+
+            Console.ReadKey();
+        }
+    }
+}
